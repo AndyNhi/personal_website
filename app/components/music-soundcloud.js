1e595b00ca93915ac5033f74645d91bbcc950929
AndyNhi <nhi.atn@gmail.com>
2015-01-23 16:21:36 -0700

    soundcloud

--- app/components/music-soundcloud.js
+++ app/components/music-soundcloud.js
@@ -2,11 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  // didInsertElement: function() {
-  //
-  //   console.log("sound")
-  //   SC.initialize({
-  //     client_id: '7afa97e60028006f28267f7f6def99ff'
-  //   });
+  didInsertElement: function() {
+
+    SC.initialize({
+      client_id: '7afa97e60028006f28267f7f6def99ff'
+    });
+
+    SC.stream("/tracks/" + track, function(sound) {
+      audio.play();
+    });
+
+  }
 
 });

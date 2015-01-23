38fb12d5fab17860ae66d84252e9b0a33e7fd1d1
AndyNhi <nhi.atn@gmail.com>
2015-01-23 14:21:02 -0700

    play and pause icon component

---
+++ app/components/music-soundcloud.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+  // didInsertElement: function() {
+  //
+  //   console.log("sound")
+  //   SC.initialize({
+  //     client_id: '7afa97e60028006f28267f7f6def99ff'
+  //   });
+
+});

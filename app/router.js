39ce49dd1c2548df6ef9b83b0de7f9ef0a8d7337
AndyNhi <nhi.atn@gmail.com>
2015-01-24 00:38:07 -0700

    link icons to blank targets

--- app/router.js
+++ app/router.js
@@ -5,11 +5,14 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+
 Router.map(function() {
   this.route('home');
   this.route('music');
   this.route('covix');
   this.route('resume');
 });
+
+
 
 export default Router;

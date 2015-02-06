6a0eed9aa2bc196b62a37ae66a525456c7bee935
AndyNhi <nhi.atn@gmail.com>
2015-02-06 09:20:13 -0700

    sc player replaced

--- app/components/music-soundcloud.js
+++ app/components/music-soundcloud.js
@@ -3,26 +3,13 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement: function() {
-
-    Ember.SC.initialize({
+    console.log("hello")
+    console.log(document.getElementById("widget"))
+    console.log(SC);
+    SC.initialize({
       client_id: ENV.soundcloudApiKey
     });
 
-    Ember.$('.music-player').on('click', 'div', function() {
-
-      if(Ember.$(this).hasClass('play-icon')) {
-        Ember.$('.music-player div').not(Ember.$(this)).removeClass('pause-icon').addClass('play-icon');
-        Ember.$(this).removeClass('play-icon').addClass('pause-icon');
-        var track = Ember.$(this).data("track");
-        Ember.SC.stream("/tracks/" + track, function(sound) {
-          Ember.soundManager.stopAll();
-          sound.play();
-        });
-      } else {
-        Ember.$(this).removeClass('pause-icon').addClass('play-icon');
-        Ember.soundManager.pauseAll();
-      }
-    });
+    SC.oEmbed("http://soundcloud.com/andy-clyde-nguyen/sets/the-groove", {color: "black"},  document.getElementById("widget"));
   }
-
 });
